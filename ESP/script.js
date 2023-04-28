9bf983215a824a2f8511fd786b751a877d1e3e76
Emir Sadović <127632361+esadovic@users.noreply.github.com>
2023-04-28 09:56:53 +0200

    Add files via upload

--- ESP/script.js
+++ ESP/script.js
@@ -72,34 +72,21 @@ function onNavChange(nav) {
   }
 }
 
+//gallery
 
-let slideIndex = 1;
-showSlides(slideIndex);
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+for (var i = 1; i <= 10; i++) {
+  var thumb = document.createElement("img");
+  thumb.src = "images/image" + i + ".jpg";
+  thumb.alt = "Image " + i;
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
+  thumb.addEventListener(
+    "click", function() {
+      currentImage.src = this.src;
+    }
+  );
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
